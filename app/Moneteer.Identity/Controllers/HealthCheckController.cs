7201bcb329fcce9ecab1eb0b993113906499bfda
Chris Oliver <cado82@gmail.com>
2019-07-19 13:03:17 +0100

    Add healthcheck controller

---
+++ app/Moneteer.Identity/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Moneteer.Identity.Controllers
+{
+    [AllowAnonymous]
+    public class HealthCheckController : Controller
+    {
+        [Route("healthcheck")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
